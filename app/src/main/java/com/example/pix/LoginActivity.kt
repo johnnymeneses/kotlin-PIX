d2f1281624f7fe08f1aeb74a4563882e56e6fa80
Johnny Meneses <bilbo_vader@outlook.com>
2022-04-23 17:27:23 -0300

    Logica Tela Login

--- app/src/main/java/com/example/pix/LoginActivity.kt
+++ app/src/main/java/com/example/pix/LoginActivity.kt
@@ -1,9 +1,17 @@
 package com.example.pix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.pix.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    //Nome da Activity no binding
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -11,6 +19,26 @@ class LoginActivity : AppCompatActivity() {
         //retirar barra superior
         supportActionBar?.hide()
 
+        //Intanciar
+        binding = ActivityLoginBinding.inflate(layoutInflater)
 
+        //Sendo usada
+        setContentView(binding.root)
+
+        //Botão via ViewBingind
+        binding.buttonEntrar.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_Entrar){
+
+            //Instanciar nova tela
+            startActivity(Intent(this, PrincipalActivity::class.java))
+
+            //Finalizar tela de login
+            finish()
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/pix/SecurityPreferences.kt
@@ -0,0 +1,16 @@
+package com.example.pix
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SecurityPreferences(context: Context) {
+
+    private val security: SharedPreferences = context.getSharedPreferences("PIX", Context.MODE_PRIVATE)
+
+
+    //salvar valor
+
+    //recuperar valor
+
+
+}
