bd956d4b7bec642983d1a0039a817a2caf680429
Johnny Meneses <bilbo_vader@outlook.com>
2022-04-23 16:55:39 -0300

    Desenho Tela Login

--- app/src/main/java/com/example/pix/LoginActivity.kt
+++ app/src/main/java/com/example/pix/LoginActivity.kt
@@ -3,15 +3,13 @@ package com.example.pix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_login)
 
         //retirar barra superior
         supportActionBar?.hide()
-
 
 
     }
