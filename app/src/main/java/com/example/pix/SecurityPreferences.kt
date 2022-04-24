079897015b4a4c9fee4816428f0f69c30ca2a937
Johnny Meneses <bilbo_vader@outlook.com>
2022-04-24 11:46:21 -0300

    Build novo

--- app/src/main/java/com/example/pix/SecurityPreferences.kt
+++ app/src/main/java/com/example/pix/SecurityPreferences.kt
@@ -5,16 +5,17 @@ import android.content.SharedPreferences
 
 class SecurityPreferences(context: Context) {
 
-    private val security: SharedPreferences = context.getSharedPreferences("PIX", Context.MODE_PRIVATE)
+    private val security: SharedPreferences =
+        context.getSharedPreferences("PIX", Context.MODE_PRIVATE)
 
 
     //salvar valor
-    fun storeString(key: String, str: String){
-        security.edit().putString(key,str).apply()
+    fun storeString(key: String, str: String) {
+        security.edit().putString(key, str).apply()
     }
 
     //recuperar valor
-    fun getString(key: String): String{
+    fun getString(key: String): String {
         return security.getString(key, "") ?: ""
         //Se este for nulo security.getString(key, "") -> retorna esse ?: ""
         //Se não for nulo, é usada essa (key, "")
